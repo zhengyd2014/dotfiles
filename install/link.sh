defb00b590fa46e9e7015f07cffcdbcd8547ddab
Fred Zheng <zhengyd2014@gmail.com>
2018-04-15 10:14:39 -0400

    add ssh config

--- install/link.sh
+++ install/link.sh
@@ -1,16 +1,30 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 DOTFILES=$HOME/dotfiles
 
+declare -A links
 echo -e "\nCreating symlinks"
 echo "=============================="
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".symlink" )"
-    if [ -e $target ]; then
-        echo "~${target#$HOME} already exists... Skipping."
+	links[$file]=$target
+done
+
+links[$HOME/dotfiles/ssh/config]=$HOME/.ssh/config
+
+
+
+# for file in "${!links[@]}" ; do
+# 	echo $file = ${links[$file]}
+# done
+# 
+
+for file in "${!links[@]}" ; do
+    if [ -e  ${links[$file]} ]; then
+        echo "~${links[$file]#$HOME} already exists... Skipping."
     else
-        echo "Creating symlink for $file"
-        ln -s $file $target
+        echo "Creating symlink for ${links[$file]}"
+        ln -s $file ${links[$file]}
     fi
 done
